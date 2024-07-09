bf9f21d2cb8881c5823c0c39b224adb779795d8e
Farid Huseynli <hseynli@list.ru>
2024-07-09 17:29:07 +0400

    Initial commit

---
+++ EfMigrations/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfMigrations;
+
+internal class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EfMigrations;Integrated Security=True;TrustServerCertificate=True;");
+    }
+}
---
+++ EfMigrations/Program.cs
@@ -0,0 +1,16 @@
+﻿using EfMigrations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
+
+using ApplicationContext db = new ApplicationContext();
+//db.Database.EnsureDeleted();
+//db.Database.EnsureCreated();
+
+// Will aplly all non-applied migrations
+await db.Database.MigrateAsync();
+
+//User user = new User { Name = "John" };
+//db.Users.Add(user);
+//db.SaveChanges();
+
+Console.WriteLine("\nDone!");
